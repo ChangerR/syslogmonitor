7d4eac2cd95509a23a9e7326fa2903653f00d01a
dingjc <dingjc@mchz.com.cn>
2016-12-28 19:15:23 +0800

    update

---
+++ logparser.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import json
+
+logfail = 'proxy sshd[27827]: Failed password for root from 218.65.30.122 port 52785 ssh2'
+class LogParser :
+    def __init__(self) :
+
+    
