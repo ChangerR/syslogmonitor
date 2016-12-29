9a9eb78a3c87412f5e32dd5a06f3b44d64d06294
changer <dingjinchengyx@163.com>
2016-12-29 23:44:45 +0800

    fix lex error

--- logparser.py
+++ logparser.py
@@ -5,32 +5,23 @@ import ply.lex as lex
 
 logfail = 'proxy sshd[27827]: Failed password for root from 218.65.30.122 port 52785 ssh2'
 
-tokens = (
-    'SSHD',
-    'LEFTARC',
-    'RIGHTARC',
-    'FAILED',
-    'PASSWORD',
-    'FOR',
-    'FROM',
-    'COLON',
-    'IP',
-    'PORT',
-    'SSH2',
-    'NUMBER',
-    'IDENTIFY',
-)
-    
+reserved = {
+    'sshd' : 'SSHD' ,
+    'Failed' : 'FAILED' ,
+    'password' : 'PASSWORD' ,
+    'for' : 'FOR' ,
+    'from' : 'FROM' ,
+    'port' : 'PORT' ,
+    'ssh2' : 'SSH2' ,
+}
+
+tokens = [
+    'IP','LBRACKET','RBRACKET','COLON','NUMBER','IDENTIFY'
+] + list (reserved.values() )
+
 t_SSHD = r'sshd'
-t_LEFTARC = r'\['
-t_RIGHTARC = r'\]'
-t_FAILED = r'Failed'
-t_PASSWORD = r'password'
-t_FOR = r'for'
-t_FROM = r'from'
-t_PORT = r'port'
-t_SSH2 = r'ssh2'
-t_IDENTIFY = r'\w+'
+t_LBRACKET = r'\['
+t_RBRACKET = r'\]'
 t_COLON = r':'
 
 def t_IP(t) :
@@ -43,6 +34,11 @@ def t_NUMBER(t):
     t.value = int(t.value)    
     return t
 
+def t_IDENTIFY(t) :
+    r'\w+'
+    t.type = reserved.get(t.value,'IDENTIFY')
+    return t
+     
 # Define a rule so we can track line numbers
 def t_newline(t):
     r'\n+'
